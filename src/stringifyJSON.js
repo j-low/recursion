a8e93b98b3349af2e908267d8e085d8af546665b
Joe Lowinske <lowi0008 (at) gmail.com>
2014-06-29 22:23:02 -0500

    stringifyJSON almost done; problems with escape chars

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,70 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if(obj === null || typeof(obj) !== "object") {
+    // handle simple object or screen undefined/function object
+    if(obj !== undefined && typeof(obj) !== 'function') {
+	  if (typeof(obj) === 'string') {
+	    obj = '"' + obj + '"';
+	  } else if(obj === null){
+	    obj = 'null';
+	  } else {
+	    obj = obj.toString();
+	  }
+	  return obj;
+	}
+  } else { 
+    // handle array or object literal
+    var finalObj = [];
+	var propCount = 0;
+	var isLastCounter = 0;	
+	var isArr = (obj instanceof Array) ? true : false;
+
+    for (var prop in obj) {
+	  propCount += 1;
+	}
+	
+    finalObj.push(isArr ? '[' : '{');
+	
+	//begin for loop
+	for (var key in obj) {
+	  //handle SIMPLE NESTED object
+	  isLastCounter += 1;
+	  var currentObj;
+	  var isLastProp = isLastCounter === propCount ? true : false;
+	  
+	  if(obj[key] === null || typeof(obj[key]) !== "object") {
+		if(obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+          if(typeof(obj[key]) === 'string' ) {
+		    obj[key] = '"' + obj[key] + '"';
+		  }
+		  if(obj[key] === null) {
+		    obj[key] = 'null';
+		  }
+		  if(typeof(obj[key]) !== 'string') {
+		    obj[key] = obj[key].toString();
+		  }
+		  obj[key] = obj[key].replace(/\s+/g, '');
+		  finalObj.push(isArr ? obj[key] : '"' + key + '":' + obj[key]);
+		}
+	  } else {
+	  //handle COMPLEX NESTED object
+	    finalObj.push('"' + key + '":');
+	  if(!isArr) {
+	  }
+	  finalObj.push(stringifyJSON(obj[key]));
+	  }
+	  if(!isLastProp) {
+	    finalObj.push(",");	
+	  }
+	}
+	
+	if(finalObj[finalObj.length-1] === ",") {
+	  finalObj = finalObj.slice(0, finalObj.length-1);
+	}
+	finalObj.push(isArr ? ']' : '}');
+	finalObj = finalObj.join('');
+	return finalObj;
+  } 
 };
+
+

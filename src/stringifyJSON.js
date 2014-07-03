1b46d668eae59587aabf08d5f9cf326a04bb5dab
Joe Lowinske <lowi0008 (at) gmail.com>
2014-07-03 11:00:19 -0500

    added comments to stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,7 @@
 var stringifyJSON = function(obj) {
 
   if(obj === null || typeof(obj) !== "object") {
-    // handle simple object or screen undefined/function object
+    // handle SIMPLE OBJECT or screen undefined/function object
     if(obj !== undefined && typeof(obj) !== 'function') {
 	  if (typeof(obj) === 'string') {
 	    obj = '"' + obj + '"';
@@ -13,7 +13,7 @@ var stringifyJSON = function(obj) {
 	  return obj;
 	}
   } else { 
-    // handle array or object literal
+    // handle ARRAY or OBJECT LITERAL
     var finalObj = [];
 	var propCount = 0;
 	var isLastCounter = 0;	
@@ -25,42 +25,52 @@ var stringifyJSON = function(obj) {
 	
     finalObj.push(isArr ? '[' : '{');
 	
-	//begin for loop
 	for (var key in obj) {
-	  //handle SIMPLE NESTED object
+	  //handle SIMPLE **NESTED** object
 	  isLastCounter += 1;
 	  var currentObj;
 	  var isLastProp = isLastCounter === propCount ? true : false;
 	  
+	  //pass all nulls, strings, and numbers
 	  if(obj[key] === null || typeof(obj[key]) !== "object") {
+		//screen all undefs and functions  
 		if(obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+		  //format strings	
           if(typeof(obj[key]) === 'string' ) {
 		    obj[key] = '"' + obj[key] + '"';
 		  }
+		  //format nulls
 		  if(obj[key] === null) {
 		    obj[key] = 'null';
 		  }
+		  //format nums (convert to strings)
 		  if(typeof(obj[key]) !== 'string') {
 		    obj[key] = obj[key].toString();
 		  }
+		  //eliminate white space in value
 		  obj[key] = obj[key].replace(/\s+/g, '');
 		  finalObj.push(isArr ? obj[key] : '"' + key + '":' + obj[key]);
 		}
 	  } else {
-	  //handle COMPLEX NESTED object
+	  //handle COMPLEX **NESTED** object
 	    finalObj.push('"' + key + '":');
 	  if(!isArr) {
 	  }
+	  //recurse nested objects
 	  finalObj.push(stringifyJSON(obj[key]));
 	  }
+	  
 	  if(!isLastProp) {
 	    finalObj.push(",");	
 	  }
 	}
 	
+	//remove last value if === ","
 	if(finalObj[finalObj.length-1] === ",") {
 	  finalObj = finalObj.slice(0, finalObj.length-1);
 	}
+	
+	//cap complex object with closing bracket/brace; collapse white space between values & return
 	finalObj.push(isArr ? ']' : '}');
 	finalObj = finalObj.join('');
 	return finalObj;

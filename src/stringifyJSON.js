9f3a3aafc5a4961538c67df75a100c357f057815
Joe Lowinske <lowi0008 (at) gmail.com>
2014-07-07 11:37:07 -0500

    stringifyJSON complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,28 @@
+
 var stringifyJSON = function(obj) {
   var isArr = (obj instanceof Array);
+  var deepCopy = function(origin) {
+    var isArray = origin instanceof Array;
+    var isObj = Object.prototype.toString.call(origin) === '[object Object]';
+    var newObj = isArray ? [] : {};
+  
+    if(isArray) {
+      //console.log('Array block called');
+      for (var i = 0; i < origin.length; i++) {
+	    var isObjOrArray = origin[i] instanceof Array || Object.prototype.toString.call(origin[i]) === '[object Object]';
+        newObj.push(isObjOrArray ? deepCopy(origin[i]) : origin[i]);   
+      }
+    } else if (origin === null) {
+	  newObj = null;
+    } else {
+	  //console.log('Object literal block called');
+      for (var key in origin) {
+	    var isObjOrArray = origin.key instanceof Array || Object.prototype.toString.call(origin.key) === '[object Object]';	
+	    newObj[key] = isObjOrArray ? deepCopy(origin[key]) : origin[key];
+ 	  }
+    }
+    return newObj;
+  };
   
   //screen undefs and functions; pass strings, nums and nulls
   if(typeof(obj) !== 'object' || obj === null) {
@@ -13,25 +36,26 @@ var stringifyJSON = function(obj) {
 	}
 	return obj;
   }  else {
-    //pass arrays, object literals and nulls.
+    //pass arrays and object literals
+	var objCopy = deepCopy(obj);
 	var finalObj = [];
 	
-	for (var key in obj) {
+	for (var key in objCopy) {
 	  var isStringifiable = false;	
-	  if (typeof(obj[key]) === 'string') {
+	  if (typeof(objCopy[key]) === 'string') {
 		  isStringifiable = true;
-	     obj[key] = '"'  + obj[key] + '"';
-	  } else if (typeof (obj[key]) === 'object' && (obj[key] !== null)) {
+	     objCopy[key] = '"'  + objCopy[key] + '"';
+	  } else if (typeof (objCopy[key]) === 'object' && (objCopy[key] !== null)) {
 		 isStringifiable = true;
-	     //recurse if obj[key] is array or object literal; screen null objects
-	    obj[key] = stringifyJSON(obj[key]);
-	  } else if (typeof(obj[key]) === 'number' || obj[key] === null || typeof(obj[key]) === 'boolean') {
+	     //recurse if objCopy[key] is array or object literal; screen null objects
+	    objCopy[key] = stringifyJSON(objCopy[key]);
+	  } else if (typeof(objCopy[key]) === 'number' || objCopy[key] === null || typeof(objCopy[key]) === 'boolean') {
 		isStringifiable = true;
-	    obj[key] = String(obj[key]);
+	    objCopy[key] = String(objCopy[key]);
 	  }
 	  
       if(isStringifiable) {
-	    finalObj.push((isArr ? '' : '"' + key + '":') + obj[key]);
+	    finalObj.push((isArr ? '' : '"' + key + '":') + objCopy[key]);
       }
 	}
 	
@@ -40,17 +64,13 @@ var stringifyJSON = function(obj) {
   }
 }; 
 
+
 var compareAll = function(arr) {
   for(var i = 0; i < arr.length; i++) {
-	console.log('OURS: ' + stringifyJSON(arr[i]));
-	console.log('THEIRS: ' + JSON.stringify(arr[i]));
+	console.log('OWN CODE: ' + stringifyJSON(arr[i]));
+	console.log('NAT CODE: ' + JSON.stringify(arr[i]));
   }	
 };
-
-
-
-
-
 
 
 
